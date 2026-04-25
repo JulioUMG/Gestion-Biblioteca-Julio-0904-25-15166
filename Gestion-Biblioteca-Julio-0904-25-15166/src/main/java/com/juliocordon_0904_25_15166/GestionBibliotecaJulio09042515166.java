43c82199db1396ec846e69683fc2b188e6367a9a
umg <umg@HUE-LAB-1F3.labhue.local>
2026-04-25 09:37:39 -0600

    He creado el proyecto y su estructura de paquetes

---
+++ Gestion-Biblioteca-Julio-0904-25-15166/src/main/java/com/juliocordon_0904_25_15166/GestionBibliotecaJulio09042515166.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.juliocordon_0904_25_15166;
+
+/**
+ *
+ * @author umg
+ */
+public class GestionBibliotecaJulio09042515166 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
